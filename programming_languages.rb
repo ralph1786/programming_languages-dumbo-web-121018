3009489c3f6dcd2cb2a338d925e60e045c0259f1
Rafael Cruz <ralph1786@gmail.com>
2018-11-26 19:10:17 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each { |type, hash|
     hash.each { |name, attributes|
-      new_languages[name] ||= attributes
-      new_languages[name][:style] ||= []
-      new_languages[name][:style] << type
+      new_hash[name] ||= attributes
+      new_hash[name][:style] ||= []
+      new_hash[name][:style] << type
     }
   }
   new_hash
