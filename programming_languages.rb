068e08ba28dbef0fd4eff90ef7ab250f08ad9458
Rafael Cruz <ralph1786@gmail.com>
2018-11-26 19:26:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.map { |language_type, hash|
+    hash.map { |name, attributes|
+      new_hash[name] ||= attributes
+      new_hash[name][:style] ||= []
+      new_hash[name][:style] << language_type
+    }
+  }
+  new_hash
 end
